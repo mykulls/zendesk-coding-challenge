bfd85e0a111dcae00b94afd92c00c7a67cd5453c
Michael Shi <michshi8@gmail.com>
2021-11-28 18:24:18 -0800

    Backend Setup

---
+++ server/api.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-loop-func */
+const express = require('express');
+
+const router = express.Router();
+const axios = require('axios');
+
+const headers = {
+  Authorization: 'Bearer 4739797315f9a5d2a1704ecf49e88f1fc5c67463867e54f37f7592c7c3035192',
+};
+
+router.get('/tickets', (req, res) => {
+  const link = 'https://zccmshi.zendesk.com/api/v2/tickets.json?page[size]=25';
+  const tickets = [];
+
+  function getPage(url) {
+    console.log(url);
+    axios.get(
+      url,
+      { headers },
+    )
+      .then((data) => {
+        tickets.push(data.data.tickets);
+
+        if (data.data.meta.has_more) {
+          getPage(data.data.links.next);
+        } else {
+          res.json(tickets);
+        }
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }
+
+  getPage(link);
+  // access token 4739797315f9a5d2a1704ecf49e88f1fc5c67463867e54f37f7592c7c3035192
+});
+
+module.exports = router;
---
+++ src/origin.js
@@ -0,0 +1,4 @@
+const port = 5000;
+const origin = `http://localhost:${port}`;
+
+module.exports = { port, origin };
