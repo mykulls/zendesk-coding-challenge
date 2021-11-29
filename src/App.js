7e88e0bf32a4192222a8c255acf7b4b597e1d42e
Michael Shi <69874869+mykulls@users.noreply.github.com>
2021-11-28 22:28:10 -0800

    Show all tickets + main ticket data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Tickets from './tickets';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <img src={logo} alt="Zendesk Logo" className="logo-img" />
+        <h1>Open Tickets</h1>
+      </header>
       <Tickets />
     </div>
   );
--- src/tickets.js
+++ src/tickets.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { origin } from './origin';
-import styles from './tickets.css';
+import './tickets.css';
 
 export default function Tickets() {
   const [tickets, setTickets] = useState([]);
@@ -10,7 +10,6 @@ export default function Tickets() {
   useEffect(() => {
     axios.get(`${origin}/tickets`)
       .then((res) => {
-        console.log(res.data);
         setTickets(res.data);
       })
       .catch((e) => {
@@ -18,25 +17,78 @@ export default function Tickets() {
       });
   }, []);
 
+  function getTimeString(date) {
+    const hours = (date.getHours() + 1) % 12;
+    const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
+    const ampm = date.getHours() < 12 ? 'AM' : 'PM';
+
+    return `${hours || 12}:${minutes} ${ampm}`;
+  }
+
+  function getDateString(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  }
+
   return (
-    <div>
-      {tickets[page].map((t) => (
+    <div className="tickets-container">
+      {tickets.length > 0 ? (
         <div>
-          {t.id}
+          <div className="card card-header">
+            <span className="ïd">ID</span>
+            <span className="subject">Subject</span>
+            <span className="requested">Date Requested</span>
+            <span className="priority">Priority</span>
+          </div>
+          {tickets[page].map((t) => (
+            <div className="card" key={t.id}>
+              <span className="ïd">{t.id}</span>
+              <span className="subject">{t.subject}</span>
+              <span className="requested">{`${getDateString(new Date(t.created_at))} ${getTimeString(new Date(t.created_at))}`}</span>
+              <span className="priority">{t.priority}</span>
+            </div>
+          ))}
+
+          <div className="page-buttons">
+            {page > 0 && (
+            <button
+              type="button"
+              onClick={() => {
+                setPage(page - 1);
+              }}
+            >
+              {'<'}
+            </button>
+            )}
+            {[...Array(tickets.length).keys()].map((i) => (
+              <button
+                type="button"
+                onClick={() => {
+                  setPage(i);
+                }}
+                key={i}
+                className={i === page && 'active'}
+              >
+                {i + 1}
+              </button>
+            ))}
+            {page < tickets.length && (
+            <button
+              type="button"
+              onClick={() => {
+                setPage(page + 1);
+              }}
+            >
+              {'>'}
+            </button>
+            )}
+          </div>
         </div>
-      ))}
-      <div className={styles.pageButtons}>
-        {[...Array(tickets.size()).keys()].map((i) => (
-          <button
-            type="button"
-            onClick={() => {
-              setPage(i);
-            }}
-          >
-            {i + 1}
-          </button>
-        ))}
-      </div>
+      ) : <p>Loading...</p>}
+
     </div>
   );
 }
